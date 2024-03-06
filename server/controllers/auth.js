e744a15b883467d56dbb44ee5d224b2944ca7829
Said Gadzhiev <saga080700@gmail.com>
2024-03-06 18:12:18 -0500

    configuring AWS and setting up email verification token

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,51 @@
+import * as config from '../config.js';
+import jwt from 'jsonwebtoken';
+import { emailTemplate } from '../helpers/email.js';
+
 export const welcome = (req, res) => {
 	console.log('found');
 	res.status(200).json({ data: '🥓 helsdsl' });
+};
+
+export const preRegister = async (req, res) => {
+	try {
+		// console.log(req.body);
+
+		const { email, password } = req.body;
+		const token = jwt.sign({ email, password }, config.JWT_SECRET, {
+			expiresIn: '1h',
+		});
+
+		config.AWSSES.sendEmail(
+			emailTemplate(
+				email,
+				`
+            <p>Please click the link below to activate your account.</p>
+                <a href="${config.CLIENT_URL}/auth/account-activate/${token}">Activate my account</a>
+            `,
+				config.REPLY_TO,
+				'Activate your account'
+			),
+			(err, data) => {
+				if (err) {
+					console.log(err);
+					return res.json({ ok: false });
+				} else {
+					console.log(data);
+					return res.json({ ok: true });
+				}
+			}
+		);
+	} catch (err) {
+		console.log(err);
+		return res.json({ error: 'error....' });
+	}
+};
+
+export const register = async (req, res) => {
+	try {
+		console.log(req.body);
+	} catch (err) {
+		return res.json({ error: 'error....' });
+	}
 };
