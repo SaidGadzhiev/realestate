0f061d21be5b09614257f92f8ec1a1cde6c3f36f
Said Gadzhiev <saga080700@gmail.com>
2024-03-07 20:49:04 -0500

    adding login functions in controllers to check the users credentials on login page

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -99,3 +99,43 @@ export const register = async (req, res) => {
 		return res.status(400).json({ error: 'error....' });
 	}
 };
+
+export const login = async (req, res) => {
+	try {
+		const { email, password } = req.body;
+
+		//find user by email
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(400).json({ error: 'Wrong email' });
+		}
+
+		console.log(user.password);
+
+		//compare passwords
+		const match = await comparePassword(password, user.password);
+		if (!match) {
+			return res.status(400).json({ error: 'Wrong password' });
+		}
+		console.log(match);
+
+		//create tokens
+		const token = jwt.sign({ _id: user._id }, config.JWT_SECRET, {
+			expiresIn: '1h',
+		});
+		const refreshToken = jwt.sign({ _id: user._id }, config.JWT_SECRET, {
+			expiresIn: '7d',
+		});
+
+		user.password = undefined;
+		user.resetCode = undefined;
+
+		return res.status(200).json({
+			token,
+			refreshToken,
+			user,
+		});
+	} catch (err) {
+		return res.status(400).json({ error: 'error....something wrong' });
+	}
+};
