d5572ab25bb2a0da1292881d24f1159661ce8db5
Said Gadzhiev <saga080700@gmail.com>
2024-03-08 22:51:25 -0500

    adding middleware and jwt refresh token function

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,12 +3,15 @@ import express from 'express';
 const router = express.Router();
 
 import * as auth from '../controllers/auth.js';
+import { requireSignIn } from '../middlewares/auth.js';
 
-router.get('/', auth.welcome);
+router.get('/', requireSignIn, auth.welcome);
 router.post('/pre-register', auth.preRegister);
 router.post('/register', auth.register);
 router.post('/login', auth.login);
 router.post('/forgot-password', auth.forgotPassword);
 router.post('/access-account', auth.accessAccount);
+router.get('/refresh-token', auth.refreshToken);
+router.get('/current-user', requireSignIn, auth.currentUser);
 
 export default router;
