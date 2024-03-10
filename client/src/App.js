9e2b636ce612a724b1d8bfc19331920d7fb79825
Said Gadzhiev <saga080700@gmail.com>
2024-03-10 12:01:38 -0400

    connecting bootstrap and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
-	return <div>hello</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/register' element={<Register />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
