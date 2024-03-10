11adbbdd85c99a688713ec94cbb590902c8007b8
Said Gadzhiev <saga080700@gmail.com>
2024-03-10 14:46:58 -0400

    adding global context, nav bar and setting up new components

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,7 @@
+import { useAuth } from '../context/auth';
+
 const Home = () => {
+	const [auth, setAuth] = useAuth();
 	return (
 		<div>
 			<h1 className='display-1 bg-primary text-light p-5'>Home</h1>
