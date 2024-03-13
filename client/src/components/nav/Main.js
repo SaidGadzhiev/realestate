5cc285753e2b8d1180f32f989ba098ca7e05fb53
Said Gadzhiev <saga080700@gmail.com>
2024-03-12 20:13:10 -0400

    adding login page and some conditional rendering

--- client/src/components/nav/Main.js
+++ client/src/components/nav/Main.js
@@ -1,35 +1,62 @@
 import { NavLink } from 'react-router-dom';
+import { useAuth } from '../../context/auth';
+import { useNavigate } from 'react-router-dom';
 
 export default function Main() {
+	const [auth, setAuth] = useAuth();
+
+	const navigate = useNavigate();
+
+	const logout = () => {
+		setAuth({ user: null, token: '', refreshToken: '' });
+		localStorage.removeItem('auth');
+		navigate('/login');
+	};
+
+	const loggedIn =
+		auth.user !== null && auth.token !== '' && auth.refreshToken !== '';
+
 	return (
 		<nav className='nav d-flex justify-content-between lead'>
 			<NavLink className='nav-link' aria-current='page' to='/'>
 				Home
 			</NavLink>
-			<NavLink className='nav-link' to='/login'>
-				Login
-			</NavLink>
-			<NavLink className='nav-link' to='/register'>
-				Register
-			</NavLink>
+			{!loggedIn ? (
+				<>
+					<NavLink className='nav-link' to='/login'>
+						Login
+					</NavLink>
+					<NavLink className='nav-link' to='/register'>
+						Register
+					</NavLink>
+				</>
+			) : (
+				''
+			)}
 
-			<div className='dropdown'>
-				<li>
-					<a className='nav-link dropdown-toggle' data-bs-toggle='dropdown'>
-						User
-					</a>
-					<ul className='dropdown-menu'>
-						<li>
-							<NavLink className='nav-link' to='/dashboard'>
-								dashboard
-							</NavLink>
-						</li>
-						<li>
-							<a className='nav-link'>Logout</a>
-						</li>
-					</ul>
-				</li>
-			</div>
+			{loggedIn ? (
+				<div className='dropdown'>
+					<li>
+						<a className='nav-link dropdown-toggle' data-bs-toggle='dropdown'>
+							{auth?.user?.name ? auth.user.name : auth.user.username}
+						</a>
+						<ul className='dropdown-menu'>
+							<li>
+								<NavLink className='nav-link' to='/dashboard'>
+									dashboard
+								</NavLink>
+							</li>
+							<li>
+								<a onClick={logout} className='nav-link'>
+									Logout
+								</a>
+							</li>
+						</ul>
+					</li>
+				</div>
+			) : (
+				''
+			)}
 		</nav>
 	);
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,80 @@
+import { useState } from 'react';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/auth';
+
 const Login = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [loading, setLoading] = useState(false);
+
+	const [auth, setAuth] = useAuth();
+
+	const navigate = useNavigate();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			setLoading(true);
+			const { data } = await axios.post(`/login`, {
+				email,
+				password,
+			});
+			if (data?.error) {
+				toast.error(data.error);
+				setLoading(false);
+			} else {
+				setAuth(data);
+				localStorage.setItem('auth', JSON.stringify(data));
+				toast.success('Please check your email to activate your account');
+				setLoading(false);
+				navigate('/');
+			}
+		} catch (err) {
+			setLoading(false);
+			if (err.response.data.error) {
+				toast.error(err.response.data.error);
+			} else {
+				toast.error('Something went wrong, try again later');
+			}
+		}
+	};
+
 	return (
 		<div>
 			<h1 className='display-1 bg-primary text-light p-5'>Login</h1>
+			<div className='container'>
+				<div className='row'>
+					<div className='col-lg-4 offset-lg-4'>
+						<form onSubmit={handleSubmit}>
+							<input
+								type='text'
+								placeholder='Enter your email'
+								className='form-control'
+								required
+								autoFocus
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+							/>
+							<input
+								type='password'
+								placeholder='Enter your password'
+								className='form-control mb-4'
+								required
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+							/>
+							<button
+								disabled={loading}
+								className='btn btn-primary col-12 mb-4'
+							>
+								{loading ? 'Waiting...' : 'Loding'}
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
