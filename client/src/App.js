4cc02bbf6d7d894ed821a3f9f0888cc276559023
Said Gadzhiev <saga080700@gmail.com>
2024-03-14 20:36:49 -0400

    adding dashboards security: PrivateRoute, to ensure no one can access it with out log in

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AccountActivate from './pages/auth/AccountActivate';
 import ForgotPassword from './pages/auth/ForgotPassword';
 import AccessAccount from './pages/auth/AccessAccount';
+import AdCreate from './pages/user/Ad/AdCreate';
 import Dashboard from './pages/user/Dashboard';
+import PrivateRoute from './components/routes/PrivateRoute';
 
 function App() {
 	return (
@@ -31,7 +33,10 @@ function App() {
 						path='/auth/access-account/:token'
 						element={<AccessAccount />}
 					/>
-					<Route path='/dashboard' element={<Dashboard />} />
+					<Route path='/' element={<PrivateRoute />}>
+						<Route path='dashboard' element={<Dashboard />} />
+						<Route path='ad/create' element={<AdCreate />} />
+					</Route>
 				</Routes>
 			</AuthProvider>
 		</BrowserRouter>
--- client/src/pages/user/Dashboard.js
+++ client/src/pages/user/Dashboard.js
@@ -1,11 +1,9 @@
 import Sidebar from '../../components/nav/Sidebar';
-import { useAuth } from '../../context/auth';
 
 export default function Dashboard() {
-	const [auth, setAuth] = useAuth();
 	return (
 		<div>
-			<h1 className='display-1 bg-primary text-light p-5'>Hello</h1>
+			<h1 className='display-1 bg-primary text-light p-5'>Dashboard</h1>
 			<Sidebar />
 		</div>
 	);
