fe0e739f06cb44adf3896f85a7c8a4faf0cff937
Said Gadzhiev <saga080700@gmail.com>
2024-03-23 13:07:22 -0400

    adding a component to render for rent/sell + css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useAuth } from '../context/auth';
 import axios from 'axios';
+import AdCard from '../components/cards/AdCard';
 
 const Home = () => {
 	const [auth, setAuth] = useAuth();
@@ -26,7 +27,27 @@ const Home = () => {
 	console.log(adsForSell, adsForRent);
 	return (
 		<div>
-			<h1 className='display-1 bg-primary text-light p-5'>Home</h1>
+			<div>
+				<h1 className='display-1 bg-primary text-light p-5'>For Sell</h1>
+				<div className='container'>
+					<div className='row'>
+						{adsForSell?.map((ad) => (
+							<AdCard ad={ad} key={ad?._id} />
+						))}
+					</div>
+				</div>
+			</div>
+
+			<div>
+				<h1 className='display-1 bg-primary text-light p-5'>For Rent</h1>
+				<div className='container'>
+					<div className='row'>
+						{adsForRent?.map((ad) => (
+							<AdCard ad={ad} key={ad?._id} />
+						))}
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
